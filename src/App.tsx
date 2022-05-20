625d4dd28d0cdc953192fac286c6372b9160a878
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-20 18:15:33 +0900

    use viewport and change background color grey.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return <div className='container'>
+    <Helmet>
+      <title>doge</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+    </Helmet>
     fuga
   </div>;
 }

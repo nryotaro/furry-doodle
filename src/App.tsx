b99d73a59068a3882dda2537ab30af1b0c22ee7d
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-20 18:20:12 +0900

    Install scss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Helmet } from 'react-helmet';
 
 function App() {
   return <div className='container'>
     <Helmet>
-      <title>doge</title>
+      <title>Demo</title>
       <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
     </Helmet>
     fuga

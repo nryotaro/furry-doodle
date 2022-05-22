7719f84485abc2ad80aa79f88e277625ed6fdc7f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-22 19:08:32 +0900

    remove helment since it is no longer necessiate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.scss';
-import { Helmet } from 'react-helmet';
 import SearchForm from './components/Search/Form';
 import SearchResults from './components/Search/Results';
 
 function App() {
   return <div className='container'>
-    <Helmet>
-      <title>Demo</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
-    </Helmet>
     <SearchForm/>
     <SearchResults/>
   </div>;

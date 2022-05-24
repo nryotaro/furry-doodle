1c2de74ac35f9d20d9beaab724337c018664fced
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-24 19:43:56 +0900

    make score cell align-right, classification center

--- src/components/Search/Results/index.tsx
+++ src/components/Search/Results/index.tsx
@@ -1,6 +1,5 @@
 import styles from './index.module.scss';
 import Editor from '../../Editor';
-import { useState } from 'react';
 import useEditor from '../../../hooks/useEditor';
 import Fragment from '../../Fragment';
 
@@ -8,39 +7,39 @@ function Results() {
   const [company, setCompany] = useEditor();
 
   return <Fragment>
-    <Editor company={company} setCompany={setCompany}/>
-    <table className={styles.results} width='100%'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Score</th>
-          <th>Classification</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr onClick={(e) => {
-          e.stopPropagation();
-         setCompany('doge'); 
-        }}>
-          <td><a href='https://sp-edge.com'>doge</a></td>
-          <td>b</td>
-          <td>Relevant</td>
-        </tr>
-        <tr onClick={(e) => console.log('foob')}>
-          <td>a</td>
-          <td>b</td>
-          <td>Irrelevant</td>
-        </tr>
-        <tr>
-          <td>a</td>
-          <td>b</td>
-          <td>-</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className={styles.container}>
+      <Editor company={company} setCompany={setCompany} />
+      <table className={styles.results} width='100%'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+            <th>Classification</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr onClick={(e) => {
+            e.stopPropagation();
+            setCompany('doge');
+          }}>
+            <td><a href='https://sp-edge.com'>doge</a></td>
+            <td className={styles.score}><span>b</span></td>
+            <td className={styles.classification}>Relevant</td>
+          </tr>
+          <tr>
+            <td>a</td>
+            <td>b</td>
+            <td>Irrelevant</td>
+          </tr>
+          <tr>
+            <td>a</td>
+            <td>b</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </Fragment>
 }
-
-
 
 export default Results;
---
+++ src/hooks/search/useSearch.ts
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Query } from '../../lib/query';
+
+
+function useSearch() {
+
+    useState<Query>();
+}
+
+export default useSearch;
